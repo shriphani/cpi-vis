384812f3d3547c367405299896d608b0352b1e82
Shriphani Palakodety <shriphanip@gmail.com>
2014-04-08 23:56:36 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cpi_vis
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cpi_vis/core_test.clj
@@ -0,0 +1,7 @@
+(ns cpi-vis.core-test
+  (:require [clojure.test :refer :all]
+            [cpi-vis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
